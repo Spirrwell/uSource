03760b375c75722ea367cad7a8f769e01522d5e9
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-07-19 06:51:16 -0700

    Static init helpers

---
+++ public/static_helpers.h
@@ -0,0 +1,58 @@
+/*
+ *
+ * static_helpers.h - Helpers for use with static initialization
+ *
+ */
+#pragma once
+
+#include <functional>
+
+/**
+ * Calls a function during static initialization
+ * @tparam fn
+ * @tparam T
+ */
+template<void(*fn)()>
+class CStaticInitWrapper
+{
+public:
+	CStaticInitWrapper()
+	{
+		fn();
+	}
+
+	~CStaticInitWrapper()
+	{
+	}
+};
+
+template<void(*fn)()>
+class CStaticDestructionWrapper
+{
+public:
+	CStaticDestructionWrapper()
+	{
+	}
+
+	~CStaticDestructionWrapper()
+	{
+		fn();
+	}
+};
+
+template<void(*fn)()>
+class CStaticInitDestroyWrapper
+{
+public:
+	CStaticInitDestroyWrapper()
+	{
+		fn();
+	}
+
+	~CStaticInitDestroyWrapper()
+	{
+		fn();
+	}
+};
+
+#define CallDuringStaticInit(x) static auto __static__ ## x ## __caller = CStaticInitWrapper<x>();
