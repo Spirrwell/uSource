9fe6f296603de54f0d521590cf0947dcb8c39489
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-07-27 01:15:52 -0700

    Update to reflection stuffs

--- public/reflection.h
+++ public/reflection.h
@@ -13,34 +13,6 @@
 #include <unordered_map>
 #include <typeindex>
 
-#if 0
-namespace reflection
-{
-	enum ETypeFlags {
-		TYPEFLAGS_CONST                 = 0b0000000000000001,
-		TYPEFLAGS_NONTRIVIAL            = 0b0000000000000010,
-		TYPEFLAGS_CLASS                 = 0b0000000000000110,
-		TYPEFLAGS_UNION                 = 0b0000000000001010,
-	};
-
-	template<typename T1, typename T2>
-	inline size_t constexpr offset_of(T1 T2::*member)
-	{
-		constexpr T2 object{};
-		return size_t(&(object.*member)) - size_t(&object);
-	}
-
-	template<class T>
-	constexpr unsigned int ComputeTypeFlags()
-	{
-		unsigned int fl = 0;
-		if(std::is_const<T>::value) fl |= TYPEFLAGS_CONST;
-		if(std::is_class<T>::value) fl |= TYPEFLAGS_CLASS;
-		if(std::is_union<T>::value) fl |= TYPEFLAGS_UNION;
-		return fl;
-	}
-}
-
 struct STypeInfo_t
 {
 	std::type_info typeinfo;
@@ -78,12 +50,32 @@ struct SMethodInfo_t
 	SFieldInfo_t params[];
 };
 
-class IObject
+namespace reflection
 {
-public:
-	virtual SFieldInfo_t* GetFieldInfo() = 0;
-	virtual SMethodInfo_t* GetMethodInfo() = 0;
-};
+	enum ETypeFlags {
+		TYPEFLAGS_CONST                 = 0b0000000000000001,
+		TYPEFLAGS_NONTRIVIAL            = 0b0000000000000010,
+		TYPEFLAGS_CLASS                 = 0b0000000000000110,
+		TYPEFLAGS_UNION                 = 0b0000000000001010,
+	};
+
+	template<typename T1, typename T2>
+	inline size_t constexpr offset_of(T1 T2::*member)
+	{
+		constexpr T2 object{};
+		return size_t(&(object.*member)) - size_t(&object);
+	}
+
+	template<class T>
+	constexpr unsigned int ComputeTypeFlags()
+	{
+		unsigned int fl = 0;
+		if(std::is_const<T>::value) fl |= TYPEFLAGS_CONST;
+		if(std::is_class<T>::value) fl |= TYPEFLAGS_CLASS;
+		if(std::is_union<T>::value) fl |= TYPEFLAGS_UNION;
+		return fl;
+	}
+}
 
 //=====================================================================================//
 /**
@@ -128,43 +120,65 @@ END_STRUCT_FIELD_INFO(TestStruct)
  * Basic class declaration macro
  * Use this to enable runtime reflection on your classes
  */
-#define DECLARE_CLASS() \
-static SFieldInfo_t g_fieldInfo[]; \
-static SMethodInfo_t g_methodInfo[];\
-SFieldInfo_t* GetFieldInfo(unsigned long long& num) override { return g_fieldInfo; } \
-SMethodInfo_t* GetMethodInfo(unsigned long long& num) override { return g_methodInfo; } \
+#define DECLARE_CLASS(_class) \
+static constexpr char* BaseClassString = #_class;\
+static SFieldInfo_t* g_fieldInfo; unsigned long g_fieldInfoCount; \
+static SMethodInfo_t* g_methodInfo; unsigned long g_methodInfoCount; \
+virtual SFieldInfo_t* GetFieldInfo(unsigned long long& num) { num = g_fieldInfoCount; return g_fieldInfo; } \
+virtual SMethodInfo_t* GetMethodInfo(unsigned long long& num) { num = g_methodInfoCount; return g_methodInfo; } \
+
+#define _DECLARE_CLASS_SAVABLE_INTERNAL(_class) \
+static SFieldInfo_t* g_saveInfo; \
+unsigned long g_saveInfoCount; \
+virtual SFieldInfo_t* GetSaveInfo(unsigned long long& num) { num = g_saveInfoCount; return g_saveInfo;}
+
+#define _DECLARE_CLASS_NETWORKED_INTERNAL(_class) \
+static SFieldInfo_t* g_networkedFields; \
+unsigned long g_networkedFieldsCount; \
+virtual SFieldInfo_t* GetSaveInfo(unsigned long long& num) { num = g_networkedFieldsCount; return g_networkedFields; };
+
+#define DECLARE_CLASS_SAVEABLE(_class) \
+DECLARE_CLASS(_class) \
+_DECLARE_CLASS_SAVABLE_INTERNAL(_class)
+
+#define DECLARE_CLASS_NETWORKED(_class) \
+DECLARE_CLASS(_class) \
+_DECLARE_CLASS_NETWORKED_INTERNAL(_class)
+
+/* Classes that are both networkable and saveable */
+#define DECLARE_CLASS_NETWORKED_SAVEABLE(_class) \
+
 //=====================================================================================//
 
-
-//=====================================================================================//
-#define BEGIN_FIELD_INFO(_class) namespace C__## _class ## __wrapper {\
+#define _BEGIN_FIELD_INFO_INTERNAL_(_class, _tablename, _countname) namespace C__## _class ## __wrapper {\
 typedef _class BaseClass; \
-constexpr char* BaseClassString = #_class;\
-static constexpr SFieldInfo_t __g__ ## _class ## _field_info[] = {
+static constexpr SFieldInfo_t __g__ ## _class ## _field_info ## _tablename [] = {
 
-#define FIELD(_type, _x) { BaseClassString, #_x, offsetof(BaseClass, _x), sizeof(BaseClass::_x), typeid(_type), reflection::ComputeTypeFlags<_type>()}
-
-#define END_FIELD_INFO(_class) };} SFieldInfo_t* _class::g_fieldInfo = (SFieldInfo_t*)C__## _class ## __wrapper::__g__ ## _class ## _field_info;
-//=====================================================================================//
-
+#define _END_FIELD_INFO_INTERNAL(_class, _tablename, _countname) };} SFieldInfo_t* _class::_tablename = (SFieldInfo_t*)C__## _class ## __wrapper::__g__ ## _class ## _field_info ## _tablename;
 
 //=====================================================================================//
-#if 0
+#define BEGIN_FIELD_INFO(_class) _BEGIN_FIELD_INFO_INTERNAL_(_class, g_fieldInfo, g_fieldInfoCount)
+
+#define FIELD(_type, _x) { BaseClass::BaseClassString, #_x, offsetof(BaseClass, _x), sizeof(BaseClass::_x), typeid(_type), reflection::ComputeTypeFlags<_type>()}
+
+#define END_FIELD_INFO(_class) _END_FIELD_INFO_INTERNAL(_class, g_fieldInfo, g_fieldInfoCount)
+
+//=====================================================================================//
+
+//=====================================================================================//
+#define BEGIN_SAVE_INFO(_class) _BEGIN_FIELD_INFO_INTERNAL_(_class, g_saveInfo, g_saveInfoCount)
+
+#define END_SAVE_INFO(_class) _END_FIELD_INFO_INTERNAL(_class, g_saveInfo, g_saveInfoCount)
+//=====================================================================================//
+
+//=====================================================================================//
 #define BEGIN_METHOD_INFO(_class) namespace C__##_class##__wrapper {\
 typedef _class BaseClass; \
-constexpr char* BaseClassString2 = #_class;\
 static constexpr SMethodInfo_t __g__ ## _class ## _method_info[] = {
 
-#define METHOD(_x) {#_x, BaseClassString2, nullptr, {}}
+#define METHOD(_x) {#_x, BaseClass::BaseClassString, nullptr, {}}
 
-#define END_METHOD_INFO(_class) };} //SMethodInfo_t* _class::g_methodInfo = (SMethodInfo_t*)C__##_class##__wrapper::__g__##_class##_method_info;
-#else
-#define BEGIN_METHOD_INFO(_class) SMethodInfo_t _class::g_methodInfo[] = {
-
-#define METHOD(_x) {#_x}
-
-#define END_METHOD_INFO(_class) };
-#endif
+#define END_METHOD_INFO(_class) };} SMethodInfo_t* _class::g_methodInfo = (SMethodInfo_t*)C__##_class##__wrapper::__g__##_class##_method_info;
 //=====================================================================================//
 
 //=====================================================================================//
@@ -176,10 +190,10 @@ static constexpr SMethodInfo_t __g__ ## _class ## _method_info[] = {
 
 //=====================================================================================//
 
-class CClass : public IObject
+class CClass
 {
 public:
-	DECLARE_CLASS();
+	DECLARE_CLASS_SAVEABLE(CClass);
 	bool m_bBool;
 	void TestFunc() {};
 };
@@ -191,4 +205,7 @@ END_FIELD_INFO(CClass)
 BEGIN_METHOD_INFO(CClass)
 	METHOD(TestFunc)
 END_METHOD_INFO(CClass)
-#endif 
+
+BEGIN_SAVE_INFO(CClass)
+
+END_SAVE_INFO(CClass)
