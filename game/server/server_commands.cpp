bb19d8003633454fcf13d6039e2b2c5c6f30d71b
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-08-05 00:29:14 -0700

    Stupid simple ent_create function

--- game/server/server_commands.cpp
+++ game/server/server_commands.cpp
@@ -5,3 +5,14 @@
 #include "tier1/tier1.h"
 #include "tier1/convar.h"
 #include "tier1/concommand.h"
+
+#include "game_shared.h"
+#include "c_base_entity.h"
+
+CONCOMMAND(ent_create, "Creates an entity", FCVAR_CHEAT)
+{
+	if(argc < 2) return;
+	CBaseEntity* player = UTIL_PlayerByIndex(1);
+	if(!player) return;
+	CBaseEntity::Create(argv[1], player->Center(), Vector(0,0,0), player->edict());
+}
