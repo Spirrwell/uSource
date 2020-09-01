71e07810ba3620aa7a4fb1a8f09be6028884bd1b
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-08-31 22:17:39 -0700

    Add r_model.cpp

---
+++ rendersystem/r_model.cpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * r_model.cpp - New type of model renderer
+ *
+ */
+#include "gl_local.h"
+#include "mathlib.h"
+#include "const.h"
+#include "r_studioint.h"
+#include "triangleapi.h"
+#include "studio.h"
+#include "engine/common/pm_local.h"
+#include "cl_tent.h"
+#include "pmtrace.h"
+#include "appframework.h"
+#include "studio_int.h"
