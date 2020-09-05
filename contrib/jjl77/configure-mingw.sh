9432e5341a0536555f67d0aa50218cd29f557f9e
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-09-04 18:55:19 -0700

    Add conf script for mingw

---
+++ contrib/jjl77/configure-mingw.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export CC="/usr/bin/i686-w64-mingw32-gcc"
+export CXX="/usr/bin/i686-w64-mingw32-g++"
+export WINRC=i686-w64-mingw32-windres
+./waf configure clangdb -T debug -8 -W --prefix=~/Games/uSource --enable-new-renderer 
