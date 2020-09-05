00db38d0526404850642a2e8c63f76768fe1383a
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-09-05 02:42:12 -0700

    Remove compile_commands before regen

--- contrib/jjl77/ide-clangdb.sh
+++ contrib/jjl77/ide-clangdb.sh
@@ -6,15 +6,19 @@ TOP=`git rev-parse --show-toplevel`
 export CC="/usr/bin/i686-w64-mingw32-gcc"
 export CXX="/usr/bin/i686-w64-mingw32-g++"
 export WINRC=i686-w64-mingw32-windres
-"$TOP/waf" clangdb -T debug -8 -W --prefix=~/Games/uSource --enable-new-renderer
-cp "$TOP/build/compile_commands.json" "$TOP/compile_commands_mingw_debug.json"
-"$TOP/waf" clangdb -T release -8 -W --prefix=~/Games/uSource --enable-new-renderer
-cp "$TOP/build/compile_commands.json" "$TOP/compile_commands_mingw_release.json"
+rm "$TOP/build/compile_commands.json"
+"$TOP/waf" configure clangdb -T debug -8 -W --prefix=~/Games/uSource --enable-new-renderer
+cp -v "$TOP/build/compile_commands.json" "$TOP/compile_commands_mingw_debug.json"
+rm "$TOP/build/compile_commands.json"
+"$TOP/waf" configure clangdb -T release -8 -W --prefix=~/Games/uSource --enable-new-renderer
+cp -v "$TOP/build/compile_commands.json" "$TOP/compile_commands_mingw_release.json"
 
 # GCC
 export CC=gcc
 export CXX=g++
-"$TOP/waf" clangdb -T debug -8 -W --prefix=~/Games/uSource --enable-new-renderer
-cp "$TOP/build/compile_commands.json" "$TOP/compile_commands_gcc_debug.json"
-"$TOP/waf" clangdb -T debug -8 -W --prefix=~/Games/uSource --enable-new-renderer
-cp "$TOP/build/compile_commands.json" "$TOP/compile_commands_gcc_release.json"
+rm "$TOP/build/compile_commands.json"
+"$TOP/waf" configure clangdb -T release -8 -W --prefix=~/Games/uSource --enable-new-renderer
+cp -v "$TOP/build/compile_commands.json" "$TOP/compile_commands_gcc_debug.json"
+rm "$TOP/build/compile_commands.json"
+"$TOP/waf" configure clangdb -T debug -8 -W --prefix=~/Games/uSource --enable-new-renderer
+cp -v "$TOP/build/compile_commands.json" "$TOP/compile_commands_gcc_release.json"
