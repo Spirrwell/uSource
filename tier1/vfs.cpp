f470410e3c3b322a56a2970a85ab7bfed9b20e44
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-09-19 22:09:24 -0700

    Added fs::ParseKeyValues, and renamed several fs functions

--- tier1/vfs.cpp
+++ tier1/vfs.cpp
@@ -3,16 +3,34 @@
 
 extern IEngineFilesystem* g_pFilesystem;
 
-char* fs::ReadFileToBuffer(const char* file, bool gamedironly)
+char* fs::ReadFileToString(const char* file, size_t& sz, bool gamedironly)
 {
+	if(!file) return nullptr;
+
 	size_t fileSize = g_pFilesystem->FileSize(file, "r");
 	FileHandle_t handle = g_pFilesystem->OpenFile(file, "r", gamedironly);
 	if(!handle) return nullptr;
 
 	char* buf = (char*)Q_malloc(fileSize+1);
 
-	g_pFilesystem->Read(handle, buf, fileSize);
+	sz = g_pFilesystem->Read(handle, buf, fileSize);
 	buf[fileSize] = 0;
+	g_pFilesystem->CloseFile(handle);
+
+	return buf;
+}
+
+void* fs::ReadFileToBuffer(const char* file, size_t& sz, bool gamedironly)
+{
+	if(!file) return nullptr;
+
+	size_t fileSize = g_pFilesystem->FileSize(file, "r");
+	FileHandle_t handle = g_pFilesystem->OpenFile(file, "r", gamedironly);
+	if(!handle) return nullptr;
+
+	void* buf = (char*)Q_malloc(fileSize);
+
+	sz = g_pFilesystem->Read(handle, buf, fileSize);
 	g_pFilesystem->CloseFile(handle);
 
 	return buf;
@@ -22,6 +40,26 @@ void fs::DestroyReadBuffer(char *buf)
 {
 	if(!buf) return;
 	Q_free(buf);
+}
+
+KeyValues fs::ReadKeyValues(const char* file, bool gamedironly)
+{
+	/* Create an empty keyvalues object */
+	KeyValues kv;
+
+	if(!file) return kv;
+
+	/* Read buffer */
+	size_t sz = 0;
+	char* buf = fs::ReadFileToString(file, sz, gamedironly);
+
+	if(!buf) return kv;
+
+	kv.ParseString(buf, false, sz);
+
+	fs::DestroyReadBuffer(buf);
+
+	return kv;
 }
 
 
--- tier1/vfs.h
+++ tier1/vfs.h
@@ -6,13 +6,18 @@
 #include "common.h"
 #include "engine_int.h"
 
+#include "public/keyvalues.h"
+
 #include "containers/string.h"
 
 BEGIN_FS_NAMESPACE
 
 IEngineFilesystem* GlobalFileSystem();
 
-char* ReadFileToBuffer(const char* file, bool gamedironly = false);
+/* Reads the file into a string */
+char* ReadFileToString(const char* file, size_t& sz, bool gamedironly = false);
+void* ReadFileToBuffer(const char* file, size_t& sz, bool gamedironly = false);
 void DestroyReadBuffer(char* buf);
+KeyValues ReadKeyValues(const char* file, bool gamedironly = false);
 
 END_FS_NAMESPACE
