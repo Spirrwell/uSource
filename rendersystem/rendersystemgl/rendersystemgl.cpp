ce698025412e3ec99ac15ee6383131535cba66bb
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-11-16 18:15:08 -0800

    Start refactor

--- rendersystem/rendersystemgl/rendersystemgl.cpp
+++ rendersystem/rendersystemgl/rendersystemgl.cpp
@@ -15,131 +15,40 @@ IRenderSystem* CreateRenderSystem()
 	return new CRenderSystem_GL();
 }
 
-void CRenderSystem_GL::Init(const render_params_t &params)
+
+void rendersystem::CRenderSystem_GL::Init()
 {
-	//g_pGLPool = g_pEngineMalloc->Mem_AllocPool("RendersystemGL", __FILE__, __LINE__);
-
-	/* GLEW Initialization */
-	glewExperimental = true;
-	GLenum res = glewInit();
-
-	if(res != GLEW_OK)
-	{
-		MsgC(red, "RendersystemGL: GLEW failed to init: %s\n", glewGetErrorString(res));
-		g_pEngineDebug->HostError("RendersystemGL: GLEW failed to init\n");
-	}
-
-	Assert(g_pEngineDebug);
-
-	if(!GLEW_VERSION_3_0)
-	{
-		MsgC(red, "RendersystemGL: Your device doesn't support OpenGL 3!! This is required for this render API implementation");
-		g_pEngineDebug->HostError("RendersystemGL: Your device doesn't support OpenGL 3!! This is required for this render API implementation");
-	}
-
-	/* Setup basic opengl params */
-	glViewport(0, 0, params.width, params.height);
-
 
 }
 
-void CRenderSystem_GL::Shutdown()
+void rendersystem::CRenderSystem_GL::Shutdown()
 {
-	//g_pEngineMalloc->Mem_FreePool(&g_pGLPool, __FILE__, __LINE__);
+
 }
 
-unsigned int GetParam(unsigned long param)
+void rendersystem::CRenderSystem_GL::SetRequestedAPI(rendersystem::RenderAPIInfo_t apiInfo)
 {
-	GLint out;
-	glGetIntegerv(param, &out);
-	return out;
+
 }
 
-
-void CRenderSystem_GL::OnParamsChanged(const render_params_t &params)
+int rendersystem::CRenderSystem_GL::NumRenderDevices()
 {
-	glViewport(0, 0, params.width, params.height);
+	return 0;
 }
 
-IVertexBuffer *CRenderSystem_GL::CreateEmptyVertexBuffer(EVertexFormat fmt, EBufferType type)
+rendersystem::RenderDeviceInfo_t rendersystem::CRenderSystem_GL::DeviceInfo(int index)
+{
+	return RenderDeviceInfo_t();
+}
+
+rendersystem::IRenderDevice *rendersystem::CRenderSystem_GL::CreateRenderDevice(int index)
 {
 	return nullptr;
 }
 
-IIndexBuffer *CRenderSystem_GL::CreateEmptyIndexBuffer(EIndexFormat fmt, EBufferType type)
+void rendersystem::CRenderSystem_GL::DestroyRenderDevice(rendersystem::IRenderDevice *dev)
 {
-	return nullptr;
-}
 
-IBuffer *CRenderSystem_GL::CreateEmptyBuffer(EBufferType type)
-{
-	return nullptr;
-}
-
-ITexture *CRenderSystem_GL::CreateEmptyTexture(ETextureFormat fmt)
-{
-	return nullptr;
-}
-
-ITexture *CRenderSystem_GL::CreateTextureFromImage(const void *img)
-{
-	return nullptr;
-}
-
-ITexture *CRenderSystem_GL::CreateRenderTarget(const char *name, ETextureFormat fmt, int width, int height, int flags)
-{
-	return nullptr;
-}
-
-IGeomShader *CRenderSystem_GL::CreateGeometryShader()
-{
-	return nullptr;
-}
-
-IVertexShader *CRenderSystem_GL::CreateVertexShader()
-{
-	return nullptr;
-}
-
-IFragShader *CRenderSystem_GL::CreateFragShader()
-{
-	return nullptr;
-}
-
-void CRenderSystem_GL::SetDepthFunc(EShaderDepthFunc func)
-{
-	glDepthFunc(gl::DepthFuncToGL(func));
-	m_depthFunc = func;
-}
-
-void CRenderSystem_GL::SetBlendEquation(EShaderBlendEq func)
-{
-	glBlendEquation(gl::BlendEquationToGL(func));
-	m_blendEquation = func;
-}
-
-void CRenderSystem_GL::SetBlendFunc(EShaderBlendFunc func)
-{
-	glBlendFunc(GL_ONE, gl::BlendFuncToGL(func));
-	m_blendFunc = func;
-}
-
-void CRenderSystem_GL::SetAlphaFunc(EShaderAlphaFunc func)
-{
-	glAlphaFunc(gl::AlphaFuncToGL(func), 0.0f);
-	m_alphaFunc = func;
-}
-
-void CRenderSystem_GL::SetStencilFunc(EShaderStencilFunc func)
-{
-	glStencilFunc(gl::StencilFuncToGL(func), m_stencilRef, m_stencilMask);
-	m_stencilFunc = func;
-}
-
-void CRenderSystem_GL::SetStencilOp(EShaderStencilOp op)
-{
-	glStencilOp(gl::StencilOpToGL(op), GL_KEEP, GL_KEEP);
-	m_stencilOp = op;
 }
 
 END_RENDERSYSTEM_NAMESPACE
