3040aa4baedfb0059320c3cd5cd66ec58389ddee
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2020-12-13 18:31:16 -0800

    refactor: Removed old lib find scripts

--- scripts/waifulib/freetype.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def configure(conf):
-    conf.env.INCLUDES_FREETYPE = [os.path.abspath(conf.env.VCPKG_DIR + '/include/'), os.path.abspath(conf.env.VCPKG_DIR + '/include/freetype/')]
-    conf.env.LIB_FREETYPE = ['freetype']
-    conf.env.LIBPATH_FREETYPE = [os.path.abspath(conf.env.VCPKG_DIR + '/lib/')]
-    conf.env.HAVE_FREETYPE = 1
